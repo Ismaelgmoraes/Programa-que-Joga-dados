83f284dadd09eef0b46a4bf9b185e253219cbc79
Ismael Moraes <ismaelgmoraes@gmail.com>
2023-04-27 21:15:33 -0300

    ~Second Commit

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-import PySimpleGUI
+import PySimpleGUI as sg
+
+sg.theme('DarkAmber')  # Dando um toque de cor
+
+# Todas as coisas dentro da tela
+layout = [[sg.Text('Texto 1')],
+          [sg.Text('Texto 2'), sg.InputText()],
+          [sg.Button('OK'), sg.Button('Cancel')]
+          ]
+
+# Criando a janela
+
+janela = sg.Window('Primeira Janela', layout)
+
+# Loop para processar os eventos e pegar os valores das entradas
+while True:
+    eventos, valores = janela.Read()
+    if eventos == sg.WIN_CLOSED or eventos == 'Cancel':
+        break
+    print('Você entrou com o valor, ', valores[0])
+janela.close()
